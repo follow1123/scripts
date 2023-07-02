9990d992bc62e1d5ad6a87465b419d87c552b75d
yf <--->
2023-07-03 01:30:51 +0800

    修改背景、系统托盘相关脚本

--- switch_bg_img.sh
+++ switch_bg_img.sh
@@ -1,1 +1,1 @@
-feh --bg-fill --randomize $HOME/images/bg_img/*
+feh --bg-fill --randomize $HOME/space/images/*
--- sys_trayer.sh
+++ sys_trayer.sh
@@ -1,6 +1,22 @@
-curPID=$(ps -ef | grep trayer | awk '/alpha 0$/{print $2}')
-if [ $curPID ]; then
-	killall trayer
-else
+#!/bin/bash
+
+# 显示系统托盘
+show(){
 	trayer --edge right --align right --width 20 --height 28 --padding 10 --margin 29 --iconspacing 20 --transparent true --tint 0xFF101010 --alpha 0 &
+}
+
+# 隐藏系统托盘
+hide(){
+	killall trayer
+}
+
+# 判断系统托盘是否在运行，并返回pid
+is_running(){
+	ps -ef | grep trayer | awk '/alpha 0$/{print $2}'
+}
+
+if [ -n "$(is_running)" ]; then
+	hide
+else
+	show
 fi
